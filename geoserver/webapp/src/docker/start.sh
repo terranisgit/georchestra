52069eb31979276daa205f61892ffad8d05302bb
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2016-01-27 17:00:26 +0100

    wip

--- geoserver/webapp/src/docker/start.sh
+++ geoserver/webapp/src/docker/start.sh
@@ -7,7 +7,5 @@ else
     echo 'Initializing datadir from georchestra/geoserver_minimal_datadir'
     unzip /tmp/datadir.zip -d /tmp
     mv /tmp/geoserver_minimal_datadir-master /var/local/geoserver
-    chown -R jetty:jetty /var/local/geoserver
+    #chown -R tomcat /var/local/geoserver
 fi
-cd /var/lib/jetty
-java -Djava.io.tmpdir=/tmp/jetty -DGEOSERVER_DATA_DIR=/var/local/geoserver/ -jar /usr/local/jetty/start.jar
